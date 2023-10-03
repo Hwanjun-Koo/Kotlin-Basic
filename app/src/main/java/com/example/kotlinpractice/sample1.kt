eb74db413458aa9d7e27c034f5c33138cb2da085
구환준/모건 <koojun99@naver.com>
2023-10-03 18:36:28 +0900

    Part 1 ~ Part 3

---
+++ app/src/main/java/com/example/kotlinpractice/sample1.kt
@@ -0,0 +1,30 @@
+package com.example.kotlinpractice
+
+fun main(){
+    val name = "John"
+    println("My name is ${name}")
+    println("Is this true? ${1 == 0}")
+
+}
+
+fun helloWorld() : Unit{
+    println("Hello World!")
+    println(add(1, 2))
+}
+
+fun add(a: Int, b : Int) : Int {
+    return a + b
+}
+
+fun hi() {
+    val a : Int = 10
+
+    var b : Int = 9
+
+    b = 100
+
+    val c = 100
+    var d = 200
+
+    var name = "John"
+}
